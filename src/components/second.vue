e5f165e7b386604dce588a0eaabe556a75d335aa
huangweiqi <huangweiqi@hzed.com>
2019-05-29 11:17:11 +0800

    third commit

---
+++ src/components/second.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>second</h1>
+    <h1><router-link :to="{ path:'first'}">{{ msg1 }}</router-link></h1>
+    <h1><router-link :to="{ path:'second'}">{{ msg2 }}</router-link></h1>
+    <h1><router-link :to="{ path:'third'}">{{ msg3 }}</router-link></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg1: 'Welcome to first',
+      msg2: 'Welcome to second',
+      msg3: 'Welcome to third'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import first from '@/components/first'
+import second from '@/components/second'
+import third from '@/components/third'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    { path: '/',
+      redirect: '/first'
+    },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/first',
+      name: 'first',
+      component: first
+    },
+    {
+      path: '/second',
+      name: 'second',
+      component: second
+    },
+    {
+      path: '/third',
+      name: 'third',
+      component: third
     }
   ]
 })
